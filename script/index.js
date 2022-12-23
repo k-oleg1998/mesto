6e7656e1723a243ee12977c7b78dedca3bb8b75b
k-oleg1998 <k-oleg1998@yandex.ru>
2022-12-23 16:45:50 +0500

    23.12.22
    fix

---
+++ script/index.js
@@ -0,0 +1,36 @@
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup')
+let close = popup.querySelector('.popup__container-close')
+let nameInfo = document.querySelector('.profile__name')
+let jobInfo = document.querySelector('.profile__job')
+let formElement =  document.querySelector('.popup__container-form')
+let nameInput = popup.querySelector('.container-form__input_profile_name')
+let jobInput = popup.querySelector('.container-form__input_profile_job')
+const submit = popup.querySelector('.container-form__submit')
+
+function popupOpen() {
+    popup.classList.add('popup_open')
+    nameInput.value = nameInfo.textContent
+    jobInput.value = jobInfo.textContent
+}
+
+function popupClose() {
+    popup.classList.remove('popup_open')
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    console.dir(nameInput.value)
+    console.dir(jobInput.value)
+
+    nameInfo.textContent = nameInput.value
+    jobInfo.textContent = jobInput.value
+    popupClose();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+editButton.addEventListener('click', popupOpen);
+
+close.addEventListener('click', popupClose);
